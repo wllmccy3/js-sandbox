4be3d50a774669574a73f09c721415248cee077c
Willie <wmccoy254@west-mec.org>
2021-02-25 09:58:30 -0700

    Color selector finished, Dark Mode Toggle WIP

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let colorSelector = document.getElementById("colorSelector");
 colorSelector.onchange = function () {
     console.log(colorSelector.value)
+    document.getElementById("colorBox").style = `background-color: ${colorSelector.value};`;
 };
+
+function toggleDarkMode() {
+    alert("TOGGLE ME, PLEASE")
+}
