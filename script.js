7324a7f43f1368f3616ecead007fa3b344804727
Willie <wmccoy254@west-mec.org>
2021-03-01 09:59:19 -0700

    Finished Dark Mode, Teleport WIP

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let colorSelector = document.getElementById("colorSelector");
 colorSelector.onchange = function () {
-    console.log(colorSelector.value)
+    console.log(colorSelector.value);
     document.getElementById("colorBox").style = `background-color: ${colorSelector.value};`;
 };
 
 function toggleDarkMode() {
-    alert("TOGGLE ME, PLEASE")
+    let content = document.getElementById("content");
+    content.classList.toggle("dark-mode");
+    //alert("TOGGLE ME, PLEASE")
+}
+
+function teleport(){
+    console.log("teleport");
+    let runaway = document.getElementById("runaway");
+    runaway.innerText = "Cringe!";
 }
